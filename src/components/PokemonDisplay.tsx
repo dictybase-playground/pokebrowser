fee212bcd596c43bd069a34cea58b6ab04374db6
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-09 13:23:09 -0500

    changed data display from unordered list to mui table

--- src/components/PokemonDisplay.tsx
+++ src/components/PokemonDisplay.tsx
@@ -1,15 +1,41 @@
-import { GetAllPokemonQuery, PokemonByTypeQuery } from "../generated/graphql"
+import { GetAllPokemonQuery } from "../generated/graphql"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from "@mui/material"
 
 interface PokemonDisplayProps {
-  data: GetAllPokemonQuery | PokemonByTypeQuery
+  data: GetAllPokemonQuery
 }
 
 export const PokemonDisplay = ({ data }: PokemonDisplayProps) => {
   return (
-    <ul>
-      {data?.pokemon_v2_pokemon.map((pokemon) => {
-        return <li key={pokemon.id}>{pokemon.name}</li>
-      })}
-    </ul>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell> ID </TableCell>
+            <TableCell> Name </TableCell>
+            <TableCell> Height (decimeter) </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data?.pokemon_v2_pokemon.map((pokemon) => {
+            return (
+              <TableRow key={pokemon.id}>
+                <TableCell>{pokemon.id}</TableCell>
+                <TableCell>{pokemon.name}</TableCell>
+                <TableCell>{pokemon.height}</TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
