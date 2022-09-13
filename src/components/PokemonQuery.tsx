4e9ed8218049fd89a9b1f47a1912a1f2c5886ba5
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-13 10:15:04 -0500

    chore: remove unused code

--- src/components/PokemonQuery.tsx
+++ src/components/PokemonQuery.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { useAtom } from "jotai"
 import { pokemonLimitIntAtom } from "../context/ArgumentsProvider"
 import { useGetAllPokemonQuery } from "../generated/graphql"
@@ -6,7 +5,7 @@ import { PokemonDisplay } from "./PokemonDisplay"
 import { PokemonDisplaySkeleton } from "./PokemonDisplaySkeleton"
 import { PokemonDisplayError } from "./PokemonDisplayError"
 import { LimitSelector } from "./LimitSelector"
-import { SelectChangeEvent, Stack } from "@mui/material"
+import { Stack } from "@mui/material"
 
 export const PokemonQuery = () => {
   const [limit] = useAtom(pokemonLimitIntAtom)
