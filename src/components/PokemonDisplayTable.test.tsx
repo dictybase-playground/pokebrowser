833a8087bbfe8bb5ed15f22860bbb96b381952e8
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-16 09:06:51 -0500

    test: add tests for PokemonDisplayTable

---
+++ src/components/PokemonDisplayTable.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react"
+import { PokemonDisplayTable } from "./PokemonDisplayTable"
+import { GetAllPokemonQuery } from "../generated/graphql"
+
+const testPokemonArray: GetAllPokemonQuery["pokemon_v2_pokemon"] = [
+  {
+    __typename: "pokemon_v2_pokemon",
+    id: 1,
+    name: "bulbasaur",
+    height: 7,
+  },
+  {
+    __typename: "pokemon_v2_pokemon",
+    id: 2,
+    name: "ivysaur",
+    height: 10,
+  },
+  {
+    __typename: "pokemon_v2_pokemon",
+    id: 3,
+    name: "venusaur",
+    height: 20,
+  },
+]
+
+describe("PokemonDisplayTable", () => {
+  it("renders a table", () => {
+    render(<PokemonDisplayTable pokemonArray={testPokemonArray} />)
+
+    expect(screen.getByRole("table")).toBeInTheDocument()
+  })
+
+  it("renders N + 1 table rows for array of length N", () => {
+    render(<PokemonDisplayTable pokemonArray={testPokemonArray} />)
+
+    expect(screen.getAllByRole("row")).toHaveLength(testPokemonArray.length + 1)
+  })
+})
