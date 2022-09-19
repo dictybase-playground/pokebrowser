670bc1217b0e89e5131e3c5283a884491c933a24
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-19 08:56:18 -0500

    refactor: render PokemonDisplayRow as child of table,

--- src/components/PokemonDisplayRow.test.tsx
+++ src/components/PokemonDisplayRow.test.tsx
@@ -1,23 +1,31 @@
 import { render, screen } from "@testing-library/react"
 import { PokemonDisplayRow } from "./PokemonDisplayRow"
 import { mockPokemon } from "../data/mockPokemon"
+import { Table, TableBody } from "@mui/material"
 
 const testPokemon = mockPokemon.pokemon_v2_pokemon[0]
-const tableBody = document.createElement("tbody")
 
 describe("PokemonDisplayRow", () => {
   it("renders a table row", () => {
-    render(<PokemonDisplayRow pokemon={testPokemon} />, {
-      container: document.body.appendChild(tableBody),
-    })
+    render(
+      <Table>
+        <TableBody>
+          <PokemonDisplayRow pokemon={testPokemon} />
+        </TableBody>
+      </Table>,
+    )
 
     expect(screen.getByRole("row")).toBeInTheDocument()
   })
 
   it("displays id, name, and height data about the given pokemon", () => {
-    render(<PokemonDisplayRow pokemon={testPokemon} />, {
-      container: document.body.appendChild(tableBody),
-    })
+    render(
+      <Table>
+        <TableBody>
+          <PokemonDisplayRow pokemon={testPokemon} />
+        </TableBody>
+      </Table>,
+    )
 
     expect(screen.getByText("1")).toBeInTheDocument()
     expect(screen.getByText("bulbasaur")).toBeInTheDocument()
