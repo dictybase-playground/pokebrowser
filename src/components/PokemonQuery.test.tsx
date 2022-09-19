1ee83f5d3b064108501fd622d5050d776282474c
kevin <kevin@DESKTOP-5827MJ5.localdomain>
2022-09-19 16:30:11 -0500

    fix: restore test file

--- src/components/PokemonQuery.test.tsx
+++ src/components/PokemonQuery.test.tsx
@@ -2,22 +2,36 @@ import { render, screen } from "@testing-library/react"
 import { PokemonQuery } from "./PokemonQuery"
 import { useGetAllPokemonQuery } from "../generated/graphql"
 import { ApolloError } from "@apollo/client"
+import { mockPokemon } from "../data/mockPokemon"
+
+jest.mock("../generated/graphql", () => {
+  const useGetAllPokemonQuery = jest.fn()
+  return { useGetAllPokemonQuery }
+})
 
 describe("PokemonQuery", () => {
+  beforeEach(() => jest.clearAllMocks())
+
   it("renders pokemon", () => {
+    ;(useGetAllPokemonQuery as jest.Mock).mockReturnValue({
+      loading: false,
+      error: false,
+      data: mockPokemon,
+    })
+
     render(<PokemonQuery />)
     expect(screen.getByText("bulbasaur")).toBeInTheDocument()
     expect(screen.getByText("ivysaur")).toBeInTheDocument()
     expect(screen.getByText("venusaur")).toBeInTheDocument()
   })
 
-  it("renders a loading state if Pokemon data is loading", async () => {
+  it("renders a loading state if Pokemon data is loading", () => {
     ;(useGetAllPokemonQuery as jest.Mock).mockReturnValue({
       loading: true,
     })
 
     render(<PokemonQuery />)
-    expect(await screen.findByTestId("skeleton-loader")).toBeInTheDocument()
+    expect(screen.getByTestId("skeleton-loader")).toBeInTheDocument()
   })
 
   it("renders an error state if Pokemon data fails to load", () => {
